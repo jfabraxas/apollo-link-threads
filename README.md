59f626489aed0d990a77c40e954880110b775c0b
jfabraxas <github@jfa.gmbh>
2022-03-02 10:12:18 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# webpack-webpack-js-org-zuv8pa
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/webpack-webpack-js-org-zuv8pa)
---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import { spawn, Pool, Worker, ModuleThread } from 'threads';
+
+function component() {
+  const element = document.createElement('div');
+
+  // Lodash, now imported by this script
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+
+  return element;
+}
+
+document.body.appendChild(component());
